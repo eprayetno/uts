3896fea42bdaee8e9e250060e4e943a183999bac
Eko Hadi Prayetno <echo.prayetno@gmail.com>
2020-04-23 18:53:24 +0700

    UTS Answer2

---
+++ answer1.php
@@ -0,0 +1,13 @@
+<h1>Operator Aritmatika</h1>
+<?php
+$angka = 8;
+
+echo "Nilai angka = $angka.</br>";
+
+$a = 2;
+echo $angka+$a."</br>";
+
+echo $angka-$a."</br>";
+
+
+?>
---
+++ answer2.php
@@ -0,0 +1,15 @@
+<h1>Variable Content</h1>
+<?php
+$nama = "Eko Hadi Prayetno";
+$umur = 32;
+
+
+echo "Nama Saya : $nama</br>";
+echo "Variable nama Saya : ";
+print_r(var_dump($nama));
+echo "</br>";
+echo "Umur saya : $umur</br>";
+echo "Variable umur Saya : ";
+print_r(var_dump($umur));
+echo $umur;
+?>
